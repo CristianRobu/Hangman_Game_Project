1259e25877ac1d64c73f5bfe42f73fbb14a65879
Cristian <cristian.robu@gmail.com>
2023-05-06 11:28:44 +0100

    declared stars array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        // 1. afiseaza la consola "Welcome to Hangman! "
+        // 2. afiseaza la consola "Cuvantul este: * * * * * "
+        // 3. cere o litera de la user "Introdu o litera"
+        // 4. citeste de la user litera si salveaza intr-o variabila
+        // 5. parcurgem cuvantul (adica folosim un for) si comparam fiecare litera din cuvant cu litera salvata
+        // 6. daca exista litera, inlocuim * de la pozitia literei din cuvant in sirul de stelute
+
+        System.out.println("Welcome to Hangman!");
+        System.out.print("Cuvantul este ");
+        String hiddenWord = "avion";
+        char[] stars = new char[hiddenWord.length()];
+//        for (int i = 0; i < stars.length ; i++) {
+//            stars[i]='*';
+//        }
+        Arrays.fill(stars,'*');
+
+        System.out.println(String.valueOf(stars));
 
 
 
