908a73eb95ab4d3b37c946964cf8c750f8e0131e
Cristian <cristian.robu@gmail.com>
2023-05-07 11:00:37 +0100

    added pictures

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +10,73 @@ public class Main {
         // 5. parcurgem cuvantul (adica folosim un for) si comparam fiecare litera din cuvant cu litera salvata
         // 6. daca exista litera, inlocuim * de la pozitia literei din cuvant in sirul de stelute
         Scanner sc = new Scanner(System.in);
+        ArrayList<Character> usedLetters = new ArrayList<>();
         Random rnd = new Random();
+        int attempts = 7;
+        ArrayList<String[]> pictures = new ArrayList<>(List.of(
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "       |\n",
+                        "       |\n",
+                        "       |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        "       |\n",
+                        "       |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        "  |    |\n",
+                        "       |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        " /|    |\n",
+                        "       |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        " /|\\   |\n",
+                        "       |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        " /|\\   |\n",
+                        " /     |\n",
+                        "       |\n",
+                        "=========\n"
+                },
+                new String[]{
+                        "  +----+\n",
+                        "  |    |\n",
+                        "  O    |\n",
+                        " /|\\   |\n",
+                        " / \\   |\n",
+                        "       |\n",
+                        "=========\n"
+                }));
         ArrayList<String> word = new ArrayList<>(Arrays.asList("perfume", "transport", "manager", "woman", "proportion", "prestige", "vegetable", "environment", "obligation", "example", "liability"));
         int index = rnd.nextInt(word.size());
         System.out.println("Welcome to Hangman!");
@@ -26,23 +89,45 @@ public class Main {
         Arrays.fill(stars, '*');
 
         System.out.println(String.valueOf(stars));
+
         while (true) {
+            boolean itsLetterFound = false;
             System.out.println("Introdu o litera: ");
             char letter = sc.next().charAt(0);
+
+            if (!usedLetters.contains(letter)) {
+                usedLetters.add(letter);
+            } else {
+                System.out.println("You have already used this letter ");
+            }
+
             for (int i = 0; i < hiddenWord.length(); i++) {
                 if (hiddenWord.charAt(i) == letter) {
                     stars[i] = letter;
+                    itsLetterFound = true;
                 }
-
-
             }
+
+            if (!itsLetterFound) {
+                attempts--;
+                System.out.println("You have " + attempts + " lives left");
+                for (String line : pictures.get(0)) {
+                    System.out.println(line);
+                }
+                pictures.remove(0);
+            }
+            if (attempts == 0 || pictures.isEmpty())
+
+                if (attempts == 0) {
+                    System.out.println("Game Over ! Try again! ");
+                    break;
+                }
             System.out.println(String.valueOf(stars));
 
             if (hiddenWord.equals(String.valueOf(stars))) {
                 System.out.println("Felicitari!");
                 break;
             }
-
         }
 
 
